4a519fa65c360b4989153625708c66065fff0d7f
Todd White <todd.white@thalion.global>
2026-07-22 09:30:54 -0400

    Oracle CFBase

---
+++ Tests/CFBase/null.m
@@ -0,0 +1,14 @@
+#include "CoreFoundation/CFBase.h"
+#include "../CFTesting.h"
+
+int main (void)
+{
+  PASS_CF (CFNullGetTypeID () != 0, "CFNullGetTypeID is registered.");
+  PASS_CF (CFGetTypeID (kCFNull) == CFNullGetTypeID (),
+    "kCFNull has the null type ID.");
+  PASS_CF (CFEqual (kCFNull, kCFNull), "kCFNull is equal to itself.");
+  PASS_CFEQ (CFCopyTypeIDDescription (CFNullGetTypeID ()), CFSTR ("CFNull"),
+    "The null type is described as CFNull.");
+
+  return 0;
+}
