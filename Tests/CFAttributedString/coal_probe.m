84d03dcab57d88bcb7051d6dad2990ef9334c56a
Todd White <todd.white@thalion.global>
2026-07-22 17:19:11 -0400

    oracle: coalesce editing probe

--- Tests/CFAttributedString/coal_probe.m
+++ Tests/CFAttributedString/coal_probe.m
@@ -8,16 +8,15 @@ main (void)
   CFRange er;
   CFMutableAttributedStringRef m = CFAttributedStringCreateMutable (NULL, 0);
   CFAttributedStringReplaceString (m, CFRangeMake (0, 0), CFSTR ("abcdef"));
+  CFAttributedStringBeginEditing (m);
   CFAttributedStringSetAttribute (m, CFRangeMake (0, 3), CFSTR ("k"),
                                   CFSTR ("v"));
   CFAttributedStringSetAttribute (m, CFRangeMake (3, 3), CFSTR ("k"),
                                   CFSTR ("v"));
+  CFAttributedStringEndEditing (m);
   CFAttributedStringGetAttributes (m, 1, &er);
-  printf ("GetAttributes(1) eff=(%ld,%ld)\n", (long)er.location,
-          (long)er.length);
-  CFAttributedStringGetAttributes (m, 4, &er);
-  printf ("GetAttributes(4) eff=(%ld,%ld)\n", (long)er.location,
-          (long)er.length);
+  printf ("after begin/end editing: GetAttributes(1) eff=(%ld,%ld)\n",
+          (long)er.location, (long)er.length);
   printf ("PROBE DONE\n");
   CFRelease (m);
   return 0;
